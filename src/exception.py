0fa6acbf195a73bc0bfd51d3c59feda7b203fe55
moniishms <moniishms@gmail.com>
2026-07-14 00:51:36 +0530

    notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -14,10 +14,4 @@ class CustomException(Exception):
         self.error_message=error_message_details(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
-    
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Logging has started")
-        raise CustomException(e,sys)
+
