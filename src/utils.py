8fc84a06d5da724a7c42d4b4ec802869dfe98bb2
moniishms <moniishms@gmail.com>
2026-07-18 18:05:26 +0530

    deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
 import dill
+import pickle
 from sklearn.metrics import r2_score
 def save_object(file_path,obj):
     try:
@@ -44,6 +45,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
